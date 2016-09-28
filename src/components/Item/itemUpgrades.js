e550555861e887da182107dbea54d29f33c2e082
gustavobr <supermario64invisivel@gmail.com>
2016-09-28 00:39:11 +0000

    Item Upgrades

---
+++ src/components/Item/itemUpgrades.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    req: 1,
+    price: 10,
+    effect: '*2'
+  },
+  {
+    req: 5,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 25,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 50,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 100,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 150,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 200,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 250,
+    price: 10,
+    effect: '*2',
+  },
+  {
+    req: 300,
+    price: 10,
+    effect: 'au',
+  },
+  {
+    req: 25,
+    price: 10,
+    effect: '*2',
+    au: true
+  },
+  {
+    req: 50,
+    price: 10,
+    effect: '*2',
+    au: true
+  },
+  {
+    req: 100,
+    price: 10,
+    effect: '*2',
+    au: true
+  },
+  {
+    req: 150,
+    price: 10,
+    effect: '*2',
+    au: true
+  },
+  {
+    req: 200,
+    price: 10,
+    effect: '*2',
+    au: true
+  },
+  {
+    req: 250,
+    price: 10,
+    effect: '*2',
+    au: true
+  },
+]
